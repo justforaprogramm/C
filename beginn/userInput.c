54d5ff71e840d31ae5756a539abb3889f4e6aa12
justforaprogramm <benjamin.jugendfeuerwehr@gmail.com>
2024-02-15 16:18:06 +0100

    fff

--- beginn/userInput.c
+++ beginn/userInput.c
@@ -5,14 +5,14 @@ int main(){
     char name[25]; //bytes
     int age;
 
-    printf("\nWhat's your name?\n\r")
-    scanf("&c" &name)
+    printf("\nWhat's your name?: ");
+    scanf("%s", &name);
 
     printf("\nHow old are you?: ");
     scanf("%d", &age);
 
+    printf("Hello %s", name);
     printf("you are %d years old", age);
-
 
     return 0;
 }
