d39951e46449db4808e77440439d0020a0e4d91f
benjamin.schmid <benjamin.schmid@luetze.de>
2024-02-15 10:21:03 +0100

    user input

---
+++ beginn/userInput.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    int age;
+
+    printf("\nHow old are you?: ");
+    scanf("%d", &age);
+
+    printf("you are %d years old", age);
+
+
+    return 0;
+}
