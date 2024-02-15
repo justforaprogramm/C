cf13f063daec55f8660f34c2627b8fcd392d3a2c
benjamin.schmid <benjamin.schmid@luetze.de>
2024-02-15 10:23:11 +0100

    input

--- beginn/userInput.c
+++ beginn/userInput.c
@@ -2,7 +2,11 @@
 
 int main(){
     
+    char name[25]; //bytes
     int age;
+
+    printf("\nWhat's your name?\n\r")
+    scanf("&c" &name)
 
     printf("\nHow old are you?: ");
     scanf("%d", &age);
