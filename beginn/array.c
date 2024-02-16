c384c151b1d89531391e0ad6a26ec50d7d2334c2
justforaprogramm <benjamin.jugendfeuerwehr@gmail.com>
2024-02-16 15:22:40 +0100

    array

--- beginn/array.c
+++ beginn/array.c
@@ -1,15 +1,64 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(void){
-    double prices[] = {5, 10, 15, 20};
+void sort(double prices[], int size_price, bool reverst);
+void printArray(double a[], int size_a);
+
+int main(void)
+{
+    double prices[] = {25, 5, 10, 7, 15, 20};
 
     int size_price = sizeof(prices) / sizeof(prices[0]);
 
-    printf("%d = [", size_price);
-    for(int i = 0; i <size_price-1; i++){
-        printf("%.2f, ", prices[i]);
-    }
-    printf("%.2lf]\n", prices[size_price-1]);
+    printArray(prices, size_price);
+
+    sort(prices, size_price, 0);
+
+    printArray(prices, size_price);
 
     return 0;
+}
+
+void sort(double a[], int size_a, bool reverst)
+{
+
+    for (int i = 0; i < size_a -1; i++)
+    {
+        for (int j = 0; j < size_a - i -1; j++)
+        {
+            if(false == reverst)
+            {
+                if (a[j] > a[j+1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+            else
+            {
+                if (a[j] < a[j+1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+
+            
+        }
+        
+    }
+    
+}
+
+void printArray(double a[], int size_a)
+{
+    //give array out
+    printf("%d = [", size_a);
+    for(int i = 0; i < size_a -1; i++)
+    {
+        printf("%.2f, ", a[i]);
+    }
+    printf("%.2lf]\n", a[size_a-1]);
 }
