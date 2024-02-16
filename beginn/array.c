57446005763f637d3df8fbc006aecfbc2765c1cf
justforaprogramm <benjamin.jugendfeuerwehr@gmail.com>
2024-02-16 14:54:41 +0100

    array

--- beginn/array.c
+++ beginn/array.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-    
+    double prices[] = {5, 10, 15, 20};
+
+    int size_price = sizeof(prices) / sizeof(prices[0]);
+
+    printf("%d = [", size_price);
+    for(int i = 0; i <size_price-1; i++){
+        printf("%.2f, ", prices[i]);
+    }
+    printf("%.2lf]\n", prices[size_price-1]);
+
     return 0;
 }
