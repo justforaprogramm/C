35a578edf5f70dc68337de7a03f2645e9d01a2f1
justforaprogramm <benjamin.jugendfeuerwehr@gmail.com>
2025-12-08 17:22:11 +0100

    übungen

--- Programmieren1/Readme.md
+++ Programmieren1/Readme.md
@@ -1,1 +1,9 @@
 # programmieren in dhbw kurs: programmieren1
+
+1) 
+as Ergebnis von
+12 durch 4 ist
+3
+
+3) 
+4 + 7 / 2 = 7
---
+++ Programmieren1/übungen/übung2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    /* number
+    */
+    float num1, num2;
+
+    printf("Give num1: ");
+    scanf("%f", &num1);
+    printf("Give num2: ");
+    scanf("%f", &num2);
+
+    printf("Result is %.2f\n", num1+num2);
+}
